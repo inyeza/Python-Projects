142ebc6fc126100e69d9cf55c19d584f3a626f4c
kodjovi Amedimele <inyeza1986@gmail.com>
2024-01-20 12:43:20 -0600

    (lowercase and uppercase built-in function)

---
+++ string-lowercase.py
@@ -0,0 +1,9 @@
+# lower()  and upper() are used to display string in lowercase and uppercase mode
+name = "Kodjovi"
+result1 = name.upper()
+
+print(result1)
+
+result2 = name.lower()
+
+print(result2)
