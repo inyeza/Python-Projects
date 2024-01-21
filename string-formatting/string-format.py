a97d7ef0b302cb2aad573d381dd4e655ce17482d
kodjovi Amedimele <inyeza1986@gmail.com>
2024-01-21 12:29:35 -0600

    working with string formating

--- string-formatting/string-format.py
+++ string-formatting/string-format.py
@@ -31,3 +31,17 @@ itemno = 567
 price = 49
 myorder = "I want {0} pieces of item number {1} for {2:.2f} dollars."
 print(myorder.format(quantity, itemno, price))
+
+# Example5
+#Also, if you want to refer to the same value more than once, use the index number:
+
+age = 36
+name = "John"
+txt = "His name is {1}. {1} is {0} years old."
+print(txt.format(age, name))
+
+#Named Indexes
+#You can also use named indexes by entering a name inside the curly brackets {carname}, but then you must use names when you pass the parameter values txt.format(carname = "Ford"):
+#Example6
+myorder = "I have a {carname}, it is a {model}."
+print(myorder.format(carname = "Ford", model = "Mustang"))
