3f5d37258fd48787cbf96ae2dfcdbcf81685568c
kodjovi Amedimele <inyeza1986@gmail.com>
2024-01-27 12:23:47 -0600

    working with function

---
+++ Functions-Modules-Packages-Workspaces/calculator.py
@@ -0,0 +1,16 @@
+def addition(num1, num2):
+    add = num1 + num2
+    return add
+
+def sub(num1, num2):
+    s = num1 - num2
+    return s
+
+def multiplication(num1, num2):
+    m = num1 * num2
+    return m
+
+print(addition(10, 5))
+print(sub(7, 2))
+print(multiplication(10, 5))
+
