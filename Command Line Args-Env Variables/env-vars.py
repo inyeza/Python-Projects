6e6d92fe3e4c736baa6e5140d4726cad1f606f24
kodjovi Amedimele <inyeza1986@gmail.com>
2024-01-27 14:25:20 -0600

    working with env vars

---
+++ Command Line Args-Env Variables/env-vars.py
@@ -0,0 +1,13 @@
+import os
+
+print(os.getenv("password"))
+
+# before executing the code on the command line we need to set first the env variables:
+# Unix-based systems
+#export API_KEY='your_api_key'
+
+# Windows
+#set API_KEY='your_api_key'
+
+# run the code using: python env-vars.py
+
